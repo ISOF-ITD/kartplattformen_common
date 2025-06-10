842e161bcb91a98ad096063c5676b2de8e0d6c8c
Per Låås <per.laas@isof.se>
2025-06-10 16:44:24 +0200

    Updated to latest Elasticsearch double nested query response structure

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # In development for tests:
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    # In server:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kartplattformen_common.settings")
     try:
         from django.core.management import execute_from_command_line
