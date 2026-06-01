47da7eb5889c5be023f3ae63cde83128d0c20459
Per Låås <per.laas@isof.se>
2026-06-01 13:42:20 +0200

    Install folke on new server

--- settings_NOTDEBUG.py
+++ settings_NOTDEBUG.py
@@ -86,7 +86,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_swagger',
+    #'rest_framework_swagger',
     'sagenkarta_rest_api',
 ]
 
