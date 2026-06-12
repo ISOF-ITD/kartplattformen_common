85273e21021e5da21fd9f1f6ddae9ef4bf69c8ba
Per Låås <per.laas@isof.se>
2026-06-12 12:51:08 +0200

    Icons for sagenkartan-norsk

--- utveckling/switch2newAPI.sh
+++ utveckling/switch2newAPI.sh
@@ -1,2 +1,5 @@
 find www -name '*.js' | xargs grep -l 'garm-test.isof' | xargs sed -i.bak s/garm-test.isof.se/ull-test.isof.se/g
 find www -name '*.js' | xargs grep -l 'garm.isof' | xargs sed -i.bak s/garm.isof.se/ull-test.isof.se/g
+# Icons for sagenkartan-norsk:
+find www -name '*.js' | xargs grep -l 'ull.isof.se/static/js-apps/sagenkarta-norsk/' | xargs sed -i.bak s,ull.isof.se/static/js-apps/sagenkarta-norsk/,sagenkartan-norsk.isof.se/,g
+
